002c759cc88cb144dd3ca78ce7a7cd1b740900fe
Imran Mujawar <lrz5@cdc.gov>
2020-08-04 16:32:56 -0400

    Add files via upload

---
+++ plumber_test_2.R
@@ -0,0 +1,14 @@
+# plumber_test.R
+
+#* Gets the country metadata for COVID dash
+#* @post /country_data
+function() {
+  fun_frame <- dget(
+  "https://raw.githubusercontent.com/imujawar/MEDAB_imran/master/get_country.R")
+
+framedf <- fun_frame()
+  
+  df <- framedf %>% mutate(timevar = Sys.time())
+  
+  return(df)
+}
